b1473f8c8b4a38a473c65272daeea61ad7820ad7
이무성 <79708688+dlantjdgkgk@users.noreply.github.com>
2023-07-21 01:22:35 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -1,5 +1,11 @@
-function solution(arr, count = 0) {
-    const temp = [...arr].map((n) => n >= 50 && !(n % 2) ? n / 2 : n < 50 && n % 2 ? n * 2 + 1 : n);
-    if (arr.every((n, i) => n === temp[i])) return count;
-    return Math.max(count, solution(temp, count + 1));
+function solution(arr, n = 0) {
+    while(!arr.every(x => (x >= 50 && x % 2 === 1) || (x < 50 && x % 2 === 0))) {
+        arr = arr.map(x => {
+            if (x >= 50 && x % 2 === 0) return x / 2;
+            if (x < 50 && x % 2 === 1) return x * 2 + 1;
+            return x;
+        })
+        n++;
+    }
+    return n;
 }
